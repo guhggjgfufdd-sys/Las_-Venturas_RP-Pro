b1baf402d10e0fbf5489e9362ad52d8a1a9b1fcd
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-09-08 16:24:46 +0300

    Show only the official Las Venturas server

--- app/src/main/java/com/samp/mobile/launcher/fragments/ServersFragment.java
+++ app/src/main/java/com/samp/mobile/launcher/fragments/ServersFragment.java
@@ -1,78 +1,56 @@
 package com.samp.mobile.launcher.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.joom.paranoid.Obfuscate;
 import com.samp.mobile.R;
 import com.samp.mobile.launcher.MainActivity;
+import com.samp.mobile.launcher.config.Config;
 import com.samp.mobile.launcher.util.ViewPagerWithoutSwipe;
+
 @Obfuscate
 public class ServersFragment extends Fragment {
 
     public final class PagerAdapter extends FragmentPagerAdapter {
-        private int pageCount;
-
-        public PagerAdapter(FragmentManager fragmentManager, int i) {
-            super(fragmentManager);
-            pageCount = i;
-            Log.d("InfoJSON", "PagerAdapter " + i);
+        public PagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
         @Override
         public Fragment getItem(int position) {
-            Log.d("InfoJSON", "PagerAdapter "+position);
-            return ServerPagesItemFragment.newInstance(position);
+            return ServerPagesItemFragment.newInstance(0);
         }
 
         @Override
         public int getCount() {
-            return pageCount;
+            return 1;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            if (position == 0) {
-                return "Favorites";
-            }
-            else if (position == 1) {
-                return "Hosted";
-            }
-
-            return "None";
+            return Config.SERVER_NAME;
         }
     }
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_servers, container, false);
+        ((MainActivity) getActivity()).hideKeyboard(getActivity());
 
-        ((MainActivity)getActivity()).hideKeyboard(getActivity());
-
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout_servers);
+        ViewPagerWithoutSwipe pager = view.findViewById(R.id.viewPager_servers);
+        pager.setAdapter(new PagerAdapter(getChildFragmentManager()));
+        tabLayout.setupWithViewPager(pager);
         return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        View view = getView();
-        if(view !=null) {
-            TabLayout tabLayout = view.findViewById(R.id.tabLayout_servers);
-            ViewPagerWithoutSwipe viewPager2 = view.findViewById(R.id.viewPager_servers);
-
-            FragmentManager fm = getChildFragmentManager();
-            PagerAdapter sa = new PagerAdapter(fm, 2);
-            viewPager2.setAdapter(sa);
-            tabLayout.setupWithViewPager(viewPager2);
-        }
     }
 }
