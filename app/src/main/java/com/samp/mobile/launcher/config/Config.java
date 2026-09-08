72a63a7ab14ef31934fa0b1f4262a889be2647b2
guhggjgfufdd-sys <guhggjgfufdd@gmail.com>
2026-09-08 16:23:12 +0300

    Lock launcher identity to Las Venturas RP

--- app/src/main/java/com/samp/mobile/launcher/config/Config.java
+++ app/src/main/java/com/samp/mobile/launcher/config/Config.java
@@ -5,4 +5,13 @@ import android.app.Activity;
 public class Config {
 
     public static Activity currentContext;
+
+    public static final String SERVER_NAME = "Las Venturas RP";
+    public static final String SERVER_HOST = "142.132.203.47";
+    public static final int SERVER_PORT = 21299;
+    public static final String SERVER_ADDRESS = SERVER_HOST + ":" + SERVER_PORT;
+
+    public static boolean isAllowedServer(String host, int port) {
+        return SERVER_HOST.equals(host) && SERVER_PORT == port;
+    }
 }
